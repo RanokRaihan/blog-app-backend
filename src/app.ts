b5300a1bf15fea07a1dc08a369e042b0cc92a192
Ranok Raihan <ranokraihan@gmail.com>
2024-12-18 23:07:23 -0500

    add authentication module; include bcryptjs and jsonwebtoken dependencies, update dev script, and configure CORS in Express

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
+import cors from "cors";
 import express, { Application } from "express";
-
+// Create Express server
 const app: Application = express();
 
+// Express configuration
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+//test route
+app.get("/", (req, res) => {
+  res.send("hurray! server is up and running!! Majje Karo!!");
+});
 export default app;
